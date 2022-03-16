7b0bf294771f7d889700a967bc278f96cfcc7c68
Debidee <olayideborah@gmail.com>
2022-03-16 16:47:28 -0700

    absolute integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - compitrs absolutr integer
+ * @n: n is an integer
+ *
+ * Return: integee
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
