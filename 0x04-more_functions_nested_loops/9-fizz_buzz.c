5c5e20e2e40cd8eb399387ce541ebeaa4cc21910
Debidee <olayideborah@gmail.com>
2022-03-18 15:17:56 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,14 @@ int main(void)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
+	printf("\n");
 
 	return (0);
 }
