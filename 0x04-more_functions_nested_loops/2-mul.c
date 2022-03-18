9f13f17a2dc4d1ce0630e0c989cdb7140ba7c409
Debidee <olayideborah@gmail.com>
2022-03-18 05:16:51 -0700

    check upper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @a - An input integer
+ * @b - An inout integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	mul = a * b;
+
+	return (mul);
+}
