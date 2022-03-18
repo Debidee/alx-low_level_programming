59f3cbce9107a6181171aecf9c706950071e5570
Debidee <olayideborah@gmail.com>
2022-03-18 05:34:44 -0700

    multiply integer

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for digit
- * @a - An input integer
- * @b - An inout integer
+ * mul - multiplies two integers
+ * @a: An input integer
+ * @b: An input integer
  * Return: Always 0
  */
 
 int mul(int a, int b)
 {
-	mul = a * b;
-
-	return (mul);
+	return (a * b);
 }
