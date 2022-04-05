2f17455aa13a1aa2546b707863ec2a7cccd7058a
Debidee <olayideborah@gmail.com>
2022-04-05 14:27:31 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _create_array - crewte array
+ * @size: size of arewy
+ * @c : input char
+ *
+ * Return:pointer or  null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	p = malloc(size * sizeof(char));
+	
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	if (size ==0)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		p[i] = c;
+		i++;
+	}
+
+	return (p);
+}
