f4e038c11f7682aa6c9db6d6ab04f64157efafca
Debidee <olayideborah@gmail.com>
2022-04-05 14:29:52 -0700

    create array ed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	p = malloc(size * sizeof(char));
-	
+
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	if (size ==0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
