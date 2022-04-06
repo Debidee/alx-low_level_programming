496707ccbd261fa13a6977defd536e0ced5fd43b
Debidee <olayideborah@gmail.com>
2022-04-06 13:45:55 -0700

    return pointeraand free ed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,22 +10,28 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid, i, j;
+	int **grid, i;
 
 	if (width <= 0 || height <= 0)
 	return (NULL);
 
 	grid = (int **)malloc(sizeof(int *) * height);
+
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+		}
 			return (NULL);
 	}
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
 	return (grid);
 }
